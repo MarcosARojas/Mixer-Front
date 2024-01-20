885daf570bfc5b72b679f3120581e15701a442db
Rojas Marcos <rojasmarcosadrian.05@gmail.com>
2024-01-20 20:15:53 -0300

    Save de avances 20/1/24

--- src/app/components/preguntas/preguntas.component.ts
+++ src/app/components/preguntas/preguntas.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 
 @Component({
-  selector: 'app-info',
+  selector: 'app-preguntas',
   standalone: true,
   imports: [
     NavComponent
   ],
-  templateUrl: './info.component.html',
-  styleUrl: './info.component.css'
+  templateUrl: './preguntas.component.html',
+  styleUrl: './preguntas.component.css'
 })
-export class InfoComponent {
+export class PreguntasComponent {
 
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    NavComponent
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
